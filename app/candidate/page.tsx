59aecf5095c5da1754eeda675821478e6e7652a7
Javier Ulises Domínguez <tu_correo_de_github@loquesea.com>
2026-06-27 10:14:11 -0600

    Remove server config from client component

--- app/candidate/page.tsx
+++ app/candidate/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-export const runtime = "nodejs";
-
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 
